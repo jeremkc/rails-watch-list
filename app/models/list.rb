c38a76a14151b10ab5b932695af17cfabadba1db
Jeremy Kwan Chung jeremykwanchung.1995@gmail.com <jeremykwanchung.1995@gmail.com>
2023-11-17 20:23:13 +0100

    Add dotenv-rails

--- app/models/list.rb
+++ app/models/list.rb
@@ -3,6 +3,8 @@ class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
 
   has_many :movies, through: :bookmarks
+
+  has_one_attached :photo
   # list.movies
 
   validates :name, presence: true, uniqueness: true
